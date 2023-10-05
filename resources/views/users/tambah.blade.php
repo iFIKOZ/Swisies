8d558f04462af322ace39e6e069d9118e739bad6
iFIKOZ <fikocool@gmail.com>
2023-10-05 15:27:41 +0700

    First Commit

---
+++ resources/views/users/tambah.blade.php
@@ -0,0 +1,1 @@
+<h1>Halaman Tambah Data</h1>
---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/coba', function () {
+    return "Coba Coba";
+});
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/user', [UserController::class,'index'])->middleware('auth')->name('user');
+
+Route::get('/user/tambah', [UserController::class,'store'])->middleware('auth')->name('user.tambah');
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
